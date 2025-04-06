51248d2e2fc0b813c4f4a9239f43e99189474695
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2025-04-06 14:29:20 +0200

    Added Web API

---
+++ WebApp/Controllers/ContainerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+    }
+}
